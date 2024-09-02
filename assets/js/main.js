0f1e3bb47ba2b857d867429ae3e5818f2f3f1981
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2024-09-02 14:00:06 -0300

    inicio do responsivo 1 erro

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//variaveis para o nabar toggler
+
+const header = document.querySelector('HEader')
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector ()
