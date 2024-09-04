885d2e1f4e2963ae3588459bf41e7e533d6542ba
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2024-09-04 11:17:50 -0300

    navbar toogle completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,28 @@
 
 //variaveis para o nabar toggler
 
-const header = document.querySelector('HEader')
+const header = document.querySelector('HEader');
 const nav = document.querySelector('nav');
-const navbarMenuBtn = document.querySelector ()
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+//variaveis para o toggle do navbar
+const navbarForm = document.querySelector('.navbar-form');
+const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
+const navbarSearchBtn = document.querySelector('.navbar-search-btn');
+
+//navbar menu toggle function
+function navIsActive() {
+    header.classList.toggle('active');
+    nav.classList.toggle('active');
+    navbarMenuBtn.classList.toggle('active');
+}
+
+navbarMenuBtn.addEventListener('click', navIsActive);
+
+
+//navbar search toggle fucntion
+
+const searchBarIsActive = () => navbarForm.classList.toggle('active');
+
+navbarSearchBtn.addEventListener('click', searchBarIsActive);
+navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
