e9ccc975f1a7379199e90c2b1bde1e31a073979d
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2024-10-17 20:24:09 -0300

    criando login

--- sql/c-street.sql
+++ sql/c-street.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 02/10/2024 às 09:09
+-- Tempo de geração: 17/10/2024 às 22:39
 -- Versão do servidor: 8.3.0
 -- Versão do PHP: 8.2.18
 
@@ -49,6 +49,8 @@ CREATE TABLE IF NOT EXISTS `tabela_filme` (
   `topicos_destaque` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT 'Topicos destaque',
   `image_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `nota_filme` decimal(3,1) DEFAULT NULL,
+  `url_filme` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `classificacao` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'pode ser Principal novo ou popular',
   PRIMARY KEY (`id_filme`),
   UNIQUE KEY `nome_filme` (`nome_filme`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -57,21 +59,21 @@ CREATE TABLE IF NOT EXISTS `tabela_filme` (
 -- Despejando dados para a tabela `tabela_filme`
 --
 
-INSERT INTO `tabela_filme` (`id_filme`, `nome_filme`, `ano_filme`, `topicos_destaque`, `image_path`, `nota_filme`) VALUES
-('1', 'O Filho do Homem', 2024, 'Aventura', 'assets\\image_movie\\filme1.jpg', 7.5),
-('10', 'Punhos de aco', 2010, 'Acao\r\nDrama', 'assets\\image_movie\\filme10.jpg', 9.5),
-('11', 'Atirando Alto', 2024, 'Drama\r\nComedia', 'assets\\image_movie\\filme11.jpg', 8.0),
-('12', 'Olhinhos do Ceu', 2020, 'Comedia\r\nMisterio', 'assets\\image_movie\\filme12.jpg', 9.0),
-('13', 'Busca pela Verdade', 2014, 'Suspense\r\nTerror', 'assets\\image_movie\\filme13.jpg', 10.0),
-('14', 'A Historia de Mung', 2006, 'Acao\r\nAventura', 'assets\\image_movie\\filme15.jpg', 10.0),
-('2', 'A Maldicao do Parque', 2022, 'Suspense', 'assets\\image_movie\\filme2.jpg', 9.5),
-('3', 'Noite Eterna Azul', 2021, 'Terror\r\nSuspense', 'assets\\image_movie\\filme3.jpg', 9.5),
-('4', 'Mr. Romero', 2000, 'Comedia\r\nAventura', 'assets\\image_movie\\filme4.jpg', 10.0),
-('5', 'O Time dos Sonhos', 2024, 'Aventura\r\nComedia', 'assets\\image_movie\\filme5.jpg', 8.0),
-('6', 'Olhos Felinos', 2024, 'Terror\r\nSuspense', 'assets\\image_movie\\filme6.jpg', 10.0),
-('7', 'Tempos de Morte', 2017, 'Suspense\r\nCrime', 'assets\\image_movie\\filme7.jpg', 8.6),
-('8', 'M.D.P. (Meninos de preto)', 2007, 'Comedia\r\nMisterio', 'assets\\image_movie\\filme8.jpg', 8.5),
-('9', 'O Desconhecido', 2018, 'Documentario', 'assets\\image_movie\\filme9.jpg', 7.0);
+INSERT INTO `tabela_filme` (`id_filme`, `nome_filme`, `ano_filme`, `topicos_destaque`, `image_path`, `nota_filme`, `url_filme`, `classificacao`) VALUES
+('1', 'O Filho do Homem', 2024, 'Aventura', 'assets\\image_movie\\filme1.jpg', 7.5, 'filho_do_homem.html', 'novo'),
+('10', 'Punhos de aco', 2010, 'Acao\r\nDrama', 'assets\\image_movie\\filme10.jpg', 9.5, 'punhos_de_aco.html', 'popular'),
+('11', 'Atirando Alto', 2024, 'Drama\r\nComedia', 'assets\\image_movie\\filme11.jpg', 8.0, 'atirando_alto.html', 'novo'),
+('12', 'Olhinhos do Ceu', 2020, 'Comedia\r\nMisterio', 'assets\\image_movie\\filme12.jpg', 9.0, 'olhinhos_de_ceu.html', 'principal'),
+('13', 'Busca pela Verdade', 2014, 'Suspense\r\nTerror', 'assets\\image_movie\\filme13.jpg', 10.0, 'busca_pela_verdade.html', 'principal'),
+('14', 'A Historia de Mung', 2006, 'Acao\r\nAventura', 'assets\\image_movie\\filme15.jpg', 10.0, 'historia_de_mung.html', 'popular'),
+('2', 'A Maldicao do Parque', 2022, 'Suspense', 'assets\\image_movie\\filme2.jpg', 9.5, 'maldicao_do_parque.html', 'principal'),
+('3', 'Noite Eterna Azul', 2021, 'Terror\r\nSuspense', 'assets\\image_movie\\filme3.jpg', 9.5, 'noite_eterna_azul.html', 'principal'),
+('4', 'Mr. Romero', 2000, 'Comedia\r\nAventura', 'assets\\image_movie\\filme4.jpg', 10.0, 'mr_romero.html', 'popular'),
+('5', 'O Time dos Sonhos', 2024, 'Aventura\r\nComedia', 'assets\\image_movie\\filme5.jpg', 8.0, 'time_dos_sonhos.html', 'novo'),
+('6', 'Olhos Felinos', 2024, 'Terror\r\nSuspense', 'assets\\image_movie\\filme6.jpg', 10.0, 'olhos_felinos.html', 'novo'),
+('7', 'Tempos de Morte', 2017, 'Suspense\r\nCrime', 'assets\\image_movie\\filme7.jpg', 8.6, 'tempo_de_morte.html', 'principal'),
+('8', 'M.D.P. (Meninos de preto)', 2007, 'Comedia\r\nMisterio', 'assets\\image_movie\\filme8.jpg', 8.5, 'mdp.html', 'popular'),
+('9', 'O Desconhecido', 2018, 'Documentario', 'assets\\image_movie\\filme9.jpg', 7.0, 'desconhecido.html', 'principal');
 
 -- --------------------------------------------------------
 
@@ -110,12 +112,13 @@ CREATE TABLE IF NOT EXISTS `tabela_qntd_assistida` (
 
 DROP TABLE IF EXISTS `tabela_usuario`;
 CREATE TABLE IF NOT EXISTS `tabela_usuario` (
-  `id_usuario` char(10) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL COMMENT 'Identificação do usuario',
-  `Nome` varchar(255) CHARACTER SET armscii8 COLLATE armscii8_bin DEFAULT NULL COMMENT 'Nome do usuario',
-  `Senha` varchar(255) CHARACTER SET armscii8 COLLATE armscii8_bin DEFAULT NULL COMMENT 'Senha do usuario',
-  `Email` varchar(255) CHARACTER SET armscii8 COLLATE armscii8_bin DEFAULT NULL COMMENT 'Email do usuario',
+  `id_usuario` int NOT NULL AUTO_INCREMENT COMMENT 'Identificação do usuario',
+  `Nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Nome do usuario',
+  `Senha` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Senha do usuario (Hash)',
+  `Email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Email do usuario',
+  `data_criacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Data de criação da conta',
   PRIMARY KEY (`id_usuario`),
-  KEY `Nome` (`Nome`)
+  UNIQUE KEY `Email` (`Email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Esta tabela é a tabela usuario principal';
 COMMIT;
 
