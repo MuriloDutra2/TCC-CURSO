8b99fac8c55305e98c9c53a57335d4ce7af5eabc
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2024-11-04 08:31:51 -0300

    arrumando conexao

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 // Configurações de conexão com o banco de dados MySQL
-$host = "localhost"; // O host do servidor MySQL (no XAMPP, é 'localhost')
-$username = "root";  // Nome de usuário do MySQL (no XAMPP, geralmente é 'root')
-$password = "";      // Senha do MySQL (no XAMPP, a senha padrão é vazia)
-$dbname = "c-street"; // Nome do seu banco de dados
+$host = "sql207.infinityfree.com"; // O host do servidor MySQL (no XAMPP, é 'localhost')
+$username = "if0_37636597";  // Nome de usuário do MySQL (no XAMPP, geralmente é 'root')
+$password = "696cIdXteVIX1l ";      // Senha do MySQL (no XAMPP, a senha padrão é vazia)
+$dbname = "if0_37636597_cstreet"; // Nome do seu banco de dados
 
 // Criar a conexão
 $conn = new mysqli($host, $username, $password, $dbname);
